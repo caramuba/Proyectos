2438f0c7bbcd080a94854882f12ea45f0a0002a5
CMunozBas <caramuba@gmail.com>
2026-02-13 15:01:30 -0300

    Crud de productos products.api.js completado

--- CRUD-fullstack/frontend/src/api/products.api.js
+++ CRUD-fullstack/frontend/src/api/products.api.js
@@ -5,3 +5,15 @@ const API_URL = "http://localhost/4000/api/products";
 export const getProductsRequest = () => {
   return axios.get(API_URL); // get al backend para obtener todos los productos
 };
+
+export const createProductRequest = (product) => {
+  return axios.post(API_URL, product); //post al backend para enviar nuevo producto
+};
+
+export const deleteProductRequest = (id) => {
+  return axios.delete(`${API_URL}/${id}`); //delete al backend para eliminar producto por id
+};
+
+export const updateProductRequest = (id, product) => {
+  return axios.put(`${API_URL}/${id}`, product); //put al backend para actualizar producto por id
+};
